eff3cfa44867c1e6e8555cf81019553c19d40113
LouKang09 <marloukong09@gmail.com>
2026-09-18 17:40:44 +0800

    Refresh PWA cache for on-map legend

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cebu-map-guide-v6';
+const CACHE='cebu-map-guide-v7';
 const ASSETS=['./','./manifest.json','./map-enhancer.css','./map-enhancer-pre.js','./map-enhancer.js'];
 
 self.addEventListener('install', event => {
