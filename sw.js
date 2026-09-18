3f76260b94d7eeb89456e923c6fe6d581cf20529
LouKang09 <marloukong09@gmail.com>
2026-09-18 16:01:00 +0800

    Refresh PWA cache for road-following map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cebu-map-guide-v3';
+const CACHE='cebu-map-guide-v4';
 const ASSETS=['./','./manifest.json','./map-enhancer.css','./map-enhancer-pre.js','./map-enhancer.js'];
 
 self.addEventListener('install', event => {
