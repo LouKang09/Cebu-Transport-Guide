c704c34f8c4d76cf3c0639c5a57dc014cd0efa68
LouKang09 <marloukong09@gmail.com>
2026-09-18 14:15:39 +0800

    Add map-enhancer-pre.js

---
+++ map-enhancer-pre.js
@@ -0,0 +1,27 @@
+(() => {
+  if (!window.L || !L.map || L.map.__cebuNavigationWrapped) return;
+  const originalMapFactory = L.map;
+
+  const wrappedMapFactory = function mapWithNavigationDefaults(id, options = {}) {
+    const enhanced = {
+      ...options,
+      zoomControl: false,
+      scrollWheelZoom: window.matchMedia('(pointer:fine)').matches,
+      doubleClickZoom: true,
+      touchZoom: true,
+      dragging: true,
+      zoomSnap: 0.25,
+      zoomDelta: 0.5,
+      wheelPxPerZoomLevel: 90,
+      minZoom: 9,
+      maxZoom: 19
+    };
+    const map = originalMapFactory.call(L, id, enhanced);
+    window.__cebuTransportMap = map;
+    window.dispatchEvent(new CustomEvent('cebu-map-ready', { detail: { map } }));
+    return map;
+  };
+
+  wrappedMapFactory.__cebuNavigationWrapped = true;
+  L.map = wrappedMapFactory;
+})();
